3ef721c4dc23b0630e042029dd80f5a0390fedb7
francescosaviano3 <Fortis46!>
2023-03-08 17:39:12 +0100

    - corretta gestione su UI della presentazione della frequenza respiratoria - separate l'estrazione delle due letture di pressione.

--- app/src/main/java/it/rialtlas/healthmonitor/LoginActivity.java
+++ app/src/main/java/it/rialtlas/healthmonitor/LoginActivity.java
@@ -1,16 +1,28 @@
 package it.rialtlas.healthmonitor;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -30,61 +42,85 @@ public class LoginActivity extends AppCompatActivity {
         mPasswordEditText = findViewById(R.id.password_edit_text);
         mLoginButton = findViewById(R.id.login_button);
 
-        mLoginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Get the username and password from the EditText views
-                String username = mUsernameEditText.getText().toString();
-                String password = mPasswordEditText.getText().toString();
+        mLoginButton.setOnClickListener(v -> {
+            // Get the username and password from the EditText views
+            String username = mUsernameEditText.getText().toString();
+            String password = mPasswordEditText.getText().toString();
 
-                // Call the LoginTask AsyncTask to send the login request
-                new LoginTask().execute(username, password);
-            }
+            // Call the makeGetRequest() method to send the login request
+            makeGetRequest();
         });
     }
 
-    private class LoginTask extends AsyncTask<String, Void, String> {
+    private void makeGetRequest() {
+        final String url = "https://192.168.6.15:8443/hr/admin/user/login";
 
-        @Override
-        protected String doInBackground(String... params) {
-            // Get the username and password from the parameters
-            String username = params[0];
-            String password = params[1];
+        // Create a custom TrustManager that includes the server's SSL/TLS certificate
+        TrustManager[] trustManagers = new TrustManager[]{
+                new X509TrustManager() {
+                    @Override
+                    public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                        // Do nothing - we don't care about client certificates
+                    }
 
-            // Build the URL for the login request
-            String url = "https://192.168.6.15:8443/hr/admin/user/login" +
-                    "?username=" + username + "&password=" + password;
+                    @Override
+                    public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                        // Check if the server's certificate is trusted
+                        try {
+                            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+                            trustManagerFactory.init((KeyStore) null);
+                            X509TrustManager defaultTrustManager = (X509TrustManager) trustManagerFactory.getTrustManagers()[0];
+                            defaultTrustManager.checkServerTrusted(chain, authType);
+                        } catch (NoSuchAlgorithmException | KeyStoreException e) {
+                            throw new CertificateException(e);
+                        }
+                    }
 
-            // Build the request using the Request.Builder class
-            Request request = new Request.Builder()
-                    .url(url)
-                    .get()
-                    .build();
+                    @Override
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return new X509Certificate[0];
+                    }
+                }
+        };
 
-            // Create a new OkHttpClient instance to send the request
-            OkHttpClient client = new OkHttpClient();
-
-            // Send the request and get the response from the server
-            try {
-                Response response = client.newCall(request).execute();
-                return response.body().string();
-            } catch (IOException e) {
-                e.printStackTrace();
-                return null;
-            }
+        // Initialize the SSLContext with our custom TrustManager
+        SSLContext sslContext = null;
+        try {
+            sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, trustManagers, new SecureRandom());
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            Log.e("LoginActivity", "Error: " + e.getMessage());
+            return;
         }
 
-        @Override
-        protected void onPostExecute(String response) {
-            if (response != null) {
-                // Handle the response from the server here
-                Log.d("Login response", response);
-                Intent login = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(login);
-            } else {
-                // Log an error message if the response is null
-                Log.e("Login response", "Response is null");
+        // Configure the OkHttpClient to use our custom TrustManager and SSLContext
+        OkHttpClient client = new OkHttpClient.Builder()
+                .sslSocketFactory(sslContext.getSocketFactory(), (X509TrustManager) trustManagers[0])
+                .build();
+
+        // Build the request and send it using the OkHttpClient
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e("LoginActivity", "Error: " + e.getMessage());
             }
-        }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String responseBody = response.body().string();
+                Log.d("LoginActivity", "Response: " + responseBody);
+                if (response.isSuccessful()) {
+                    Intent b = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(b);
+                } else {
+                    Log.e("LoginActivity", "Error: Response code: " + response.code());
+                }
+            }
+        });
     }
 }
