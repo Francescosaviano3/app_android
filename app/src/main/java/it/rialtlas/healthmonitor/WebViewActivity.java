1db5c3ed4e4244f6e24093033305ed7fb976299f
francescosaviano <f.saviano3@studenti.unisa.it>
2023-05-08 12:12:26 +0200

    menu_home aggiunto nella webview

--- app/src/main/java/it/rialtlas/healthmonitor/WebViewActivity.java
+++ app/src/main/java/it/rialtlas/healthmonitor/WebViewActivity.java
@@ -1,17 +1,26 @@
 package it.rialtlas.healthmonitor;
 
+import static it.rialtlas.healthmonitor.R.menu.menu_home;
+import static it.rialtlas.healthmonitor.R.menu.onco_support_menu;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 
+import it.rialtlas.healthmonitor.view.Logout;
+
 public class WebViewActivity extends AppCompatActivity {
 
     private WebView webView;
-    private Button homeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +34,7 @@ public class WebViewActivity extends AppCompatActivity {
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("https://www.google.it");
 
-        homeButton = findViewById(R.id.home_button);
-        homeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+
     }
 
     @Override
@@ -41,5 +44,52 @@ public class WebViewActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //
+        if(id == R.id.logoutButton) {
+
+            Intent intentlogout= new Intent(this, Logout.class);
+            startActivity(intentlogout);
+            return true;
+        }
+
+
+        if (id == R.id.action_questionnaire) {
+            Intent intent = new Intent(this, WebViewActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        if (id == R.id.action_feedback) {
+            Intent emailIntent = new Intent(this, Segnalazione.class);
+            startActivity(emailIntent);
+            return true;
+        }
+
+        if (id == R.id.action_faq) {
+            Intent intent = new Intent(this, FAQActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        if (id == R.id.homepage) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
